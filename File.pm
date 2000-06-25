91f0da1d314735ad83096c27d2e8000ee420fbf7
T.J. Mather <tjmather@maxmind.com>
2000-06-25 02:46:26 +0000

    Import of TJMATHER/DBIx-FullTextSearch-0.50 from CPAN.
    gitpan-cpan-distribution: DBIx-FullTextSearch
    gitpan-cpan-version:      0.50
    gitpan-cpan-path:         TJMATHER/DBIx-FullTextSearch-0.50.tar.gz
    gitpan-cpan-author:       TJMATHER
    gitpan-cpan-maturity:     released

---
+++ File.pm
@@ -0,0 +1,26 @@
+
+package DBIx::FullTextSearch::File;
+use DBIx::FullTextSearch::String;
+use strict;
+use vars qw! @ISA !;
+@ISA = qw! DBIx::FullTextSearch::String !;
+
+sub index_document {
+	my ($self, $file) = @_;
+	my $dbh = $self->{'dbh'};
+
+	open FILE, $file or do {
+		$self->{'errstr'} = "Reading the file `$file' failed: $!";
+		return;
+		};
+	my $data;
+	{
+		local $/ = undef;
+		$data = <FILE>;
+	}
+	close FILE;
+	$self->SUPER::index_document($file, $data);
+	}
+
+1;
+
---
+++ blib/lib/DBIx/FullTextSearch/TestConfig.pm
@@ -0,0 +1,6 @@
+%DBIx::FullTextSearch::TestConfig::Config = (
+                                              'dsn' => 'dbi:mysql:tsweb',
+                                              'password' => 'max88next',
+                                              'user' => 'tsweb'
+                                            );
+1;
